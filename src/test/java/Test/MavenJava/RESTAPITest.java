d541b0f2d78962b9610566cbc2d5cdd45e380d23
pavithra <pavithra.shetty1993@gmail.com>
2021-06-03 22:18:38 +0530

    this is commit from asian timezone

--- src/test/java/Test/MavenJava/RESTAPITest.java
+++ src/test/java/Test/MavenJava/RESTAPITest.java
@@ -4,10 +4,14 @@ import org.testng.annotations.Test;
 
 public class RESTAPITest {
 	
+	
+	
 	@Test
 	public void postJira()
 	{
 		System.out.println("postjira");
+		System.out.println("postjira1");
+		System.out.println("postjira2");
 	}
 	@Test
 	public void deleteTwitter()
